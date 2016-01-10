d6dffd6f4d070597af060084fba6e0c05223a896
Chance Zibolski <chance.zibolski@gmail.com>
2016-01-10 15:13:41 -0800

    docker: Use dirname to allow running script from different directory

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # verbose/exit on error
 set -xe
-docker build -t mozilla/heka_base ..
-docker build --rm -t mozilla/heka_build .
+docker build -t mozilla/heka_base $(dirname $0)/..
+docker build --rm -t mozilla/heka_build $(dirname $0)
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti mozilla/heka_build
 docker rmi mozilla/heka_build
